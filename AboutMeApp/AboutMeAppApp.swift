2dcdca9475a4d474b646ac2f6c35be78bebe3629
Ipoetzscher <india.poetzscher@sfuhs.org>
2023-04-22 12:02:40 -0700

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by India Poetzscher on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
