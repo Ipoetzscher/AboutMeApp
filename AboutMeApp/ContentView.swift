d5393be2f63cb0c067401aba19ec89fde7d686c6
Ipoetzscher <ikpjnk@gmail.com>
2025-04-13 10:37:44 -0700

    Created initial iteration of app

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var funFacts = ["I love to swim", "I don't like chocolate", "I'm fluent in Mandarin", "I lived in Singapore for 5 years", "I can name all the flags and capitals of the world", "Sushi is my fav food and my dog's name", "I love to draw and paint"]
+    @State private var funFact = ""
+    @State private var buttonText = "click for a fun fact!"
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack() {
+            Color(.blue)
+                .ignoresSafeArea()
+            VStack(spacing: 20){
+                HStack {
+                    Image(systemName: "star")
+                    Text("India Poetzscher")
+                        .font(.largeTitle)
+                        .fontWeight(.thin)
+                    Image(systemName: "star")
+                }.foregroundColor(.white)
+                Image("beach")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .border(.blue, width: 10)
+                Button(buttonText) {
+                    let randomInt = Int.random(in: 0..<funFacts.count)
+                    funFact = funFacts[randomInt]
+                    buttonText = "click for another fact"
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 10)
+                .background(.white)
+                .cornerRadius(10)
+                .foregroundColor(.blue)
+                Text(funFact)
+                    .foregroundColor(.white)
+            }
             .padding()
+        }
     }
 }
 
